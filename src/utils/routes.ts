e449ac49ca4416f4dc153fd11c8b53cf5f4338ab
Tiven <vairtor@gmail.com>
2023-03-12 01:42:59 -0300

    Added functionality to logo of return to home page

---
+++ src/utils/routes.ts
@@ -0,0 +1,6 @@
+export const NavItems = [
+    { name: "Home", path: "/" },
+    { name: "My Stories", path: "/my-stories" },
+    { name: "Settings", path: "/settings" },
+    { name: "My Projects", path: "/my-projects" },
+  ];
