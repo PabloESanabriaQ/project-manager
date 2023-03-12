0fbd101a4ab38bb09703a1c6bd7af340126d7ba1
Tiven <vairtor@gmail.com>
2023-03-12 01:31:29 -0300

    Fixed no rendering breadcrumb on desktop

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -1,9 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 export default function BreadCrumb() {
-  const path = window.location.pathname;
+  const location = useLocation();
+
   return (
     <section className="w-1/2 h-20 min-h-fit flex items-center justify-center">
       <span className="text-2xl font-semibold text-white">
-        {path.localeCompare("/") === 0 ? "/home" : path}
+        {location.pathname.localeCompare("/")=== 0 ? "/home" : location.pathname}
       </span>
     </section>
   );
