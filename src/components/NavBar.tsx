32a9528a6ca6d4919bffbb005419bd04156f7e36
Pablo <pabloesanabriaq@gmail.com>
2023-03-11 22:05:25 -0300

    Added styles for desktop navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,11 +18,17 @@ export default function NavBar({ open, setOpen }: NavBarProps) {
       className={`w-screen h-screen fixed flex flex-col justify-evenly items-center bg-slate-500
       overflow-hidden overflow-x-hidden
       transform ease-in-out duration-500 origin-left translate-x-0
+      lg:w-1/6 lg:left-0 lg:justify-around
       ${open ? "right-0" : "right-full"}`}
     >
       <img
+        onClick={() => setOpen(!open)}
+        src={chevronLeft}
+        className="w-12 h-12 absolute m-4 top-0 left-0 lg:hidden"
+      />
+      <img
         src="https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-2.png"
-        className="w-2/6 md:w-1/6"
+        className="w-2/6 lg:w-4/6"
         alt="logo"
       />
       <ul className="w-full flex flex-col items-center gap-12">
@@ -37,11 +43,6 @@ export default function NavBar({ open, setOpen }: NavBarProps) {
       <a href="#">
         <span>MY PROFILE</span>
       </a>
-      <img
-        onClick={() => setOpen(!open)}
-        src={chevronLeft}
-        className="w-12 h-12 absolute m-4 top-0 left-0"
-      />
     </nav>
   );
 }
