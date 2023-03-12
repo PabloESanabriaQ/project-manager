854acd0050d59b960872a016b3f1c3861609f428
Tiven <vairtor@gmail.com>
2023-03-12 16:55:27 -0300

    Fix breadcrumb alignment

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -4,7 +4,7 @@ export default function BreadCrumb() {
   const location = useLocation();
 
   return (
-    <section className="w-full h-20 min-h-fit flex items-center justify-start">
+    <section className="py-5 w-full h-20 min-h-fit flex items-center justify-start">
         <span className="mx-20 lg:mx-10 px-8 py-1 text-2xl text-white bg-slate-800 text-center flex justify-center rounded-full">
           {location.pathname.localeCompare("/")=== 0 ? "/home" : location.pathname}
         </span>
